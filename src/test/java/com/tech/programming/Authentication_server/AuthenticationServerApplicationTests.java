936b65f35d39733bd3331382b491d373bc9d27f8
Rathan Sai <palakonda.rathan@synlabs.io>
2024-09-11 15:53:49 +0530

    start

---
+++ src/test/java/com/tech/programming/Authentication_server/AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.programming.Authentication_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
