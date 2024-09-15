39f2b018fb1d88ddd0f04543f555b0e0349e3f86
Rathan Sai <palakonda.rathan@synlabs.io>
2024-09-15 20:45:24 +0530

    next settings

--- src/main/java/com/tech/programming/Authentication_server/config/SecurityConfig.java
+++ src/main/java/com/tech/programming/Authentication_server/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.tech.programming.Authentication_server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,12 +16,35 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+        http.csrf(customizer->customizer.disable())
+                .authorizeHttpRequests(authorize-> authorize.anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+//        The below code is how the http using the Customizer for more read the official document.......
+//        Customizer<CsrfConfigurer<HttpSecurity>> customizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+//                httpSecurityCsrfConfigurer.disable();
+//            }
+//        };
+//
+//        http.csrf(customizer);
+//
+//        Customizer<HttpBasicConfigurer<HttpSecurity>> configurerCustomizer = new Customizer<HttpBasicConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(HttpBasicConfigurer<HttpSecurity> httpSecurityHttpBasicConfigurer) {
+//                httpSecurityHttpBasicConfigurer.disable();
+//            }
+//        };
+//
+//        http.httpBasic(configurerCustomizer);
+
         return http.build();
     }
 }
--- src/main/java/com/tech/programming/Authentication_server/controller/UserAuthController.java
+++ src/main/java/com/tech/programming/Authentication_server/controller/UserAuthController.java
@@ -1,13 +1,14 @@
 package com.tech.programming.Authentication_server.controller;
 
 import com.tech.programming.Authentication_server.dto.LoginRequest;
+import com.tech.programming.Authentication_server.dto.UserRequest;
 import com.tech.programming.Authentication_server.dto.UserResponse;
 import com.tech.programming.Authentication_server.entity.User;
 import com.tech.programming.Authentication_server.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -17,8 +18,23 @@ public class UserAuthController {
     UserService userService;
 
     @GetMapping
-    public String login(){
+    public String login(HttpServletRequest httpServletRequest){
 //        return userService.login(loginRequest);
-        return "welcome to the programming world";
+        return "welcome to the programming world " + httpServletRequest.getSession().getId();
+    }
+
+    @GetMapping("names")
+    public String get(){
+        return "rathan, jeevan";
+    }
+
+    @GetMapping("csrf-token")
+    public CsrfToken getToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("students")
+    public String add(@RequestBody UserRequest request){
+        return "added students";
     }
 }
